f59fdd51c1794e24692786539f7783692cc4e302
thelonewolf123 <harishkumar.vellore@gmail.com>
2023-06-04 19:39:05 +0530

    prototyping with ts

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack')
+const path = require('path')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+    entry: {
+        server: path.resolve(__dirname, 'src/index.ts')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: [/node_modules/],
+                loader: 'ts-loader'
+            }
+        ]
+    },
+    resolve: { extensions: ['.ts'] },
+    output: {
+        chunkFilename: '[name].js',
+        filename: '[name].js'
+    },
+
+    mode: 'development',
+    plugins: [new UglifyJSPlugin()],
+    devtool: 'source-map'
+}
