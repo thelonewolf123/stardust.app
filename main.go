9269fbaff78e3f04705c03dfa19378c84feb1090
thelonewolf123 <harishkumar.vellore@gmail.com>
2023-06-04 10:12:47 +0530

    env setup working

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("Hello, world!")
+	godotenv.Load("./.env.local")
+	fmt.Println("AWS_KEY", os.Getenv("AWS_KEY"))
 }
