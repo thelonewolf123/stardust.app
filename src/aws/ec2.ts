7df654a8430f8e5aac6310bb2826a92e3d253c27
thelonewolf123 <harishkumar.vellore@gmail.com>
2023-06-06 01:01:06 +0530

    fix

--- src/aws/ec2.ts
+++ src/aws/ec2.ts
@@ -1,1 +1,10 @@
-console.log('Hello, world!')
+import { EC2Client } from '@aws-sdk/client-ec2'
+
+const client = new EC2Client({
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+        secretAccessKey: process.env.AWS_ACCESS_KEY_SECRET || ''
+    }
+})
+
+// client.send
