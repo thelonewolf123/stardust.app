816930085738fc2f2f4b86549c998a753f79281a
thelonewolf123 <harishkumar.vellore@gmail.com>
2023-06-06 09:58:10 +0530

    fix

--- src/aws/ec2.ts
+++ src/aws/ec2.ts
@@ -1,4 +1,4 @@
-import { EC2Client } from '@aws-sdk/client-ec2'
+import { EC2Client, RequestSpotInstancesCommand } from '@aws-sdk/client-ec2'
 
 const client = new EC2Client({
     credentials: {
@@ -7,4 +7,21 @@ const client = new EC2Client({
     }
 })
 
-// client.send
+async function createEc2Instance(count: number) {
+    const input = {
+        InstanceCount: 5,
+        LaunchSpecification: {
+            IamInstanceProfile: {
+                Arn: 'arn:aws:iam::123456789012:instance-profile/my-iam-role'
+            },
+            ImageId: 'ami-1a2b3c4d',
+            InstanceType: 'm3.medium',
+            SecurityGroupIds: ['sg-1a2b3c4d'],
+            SubnetId: 'subnet-1a2b3c4d'
+        },
+        SpotPrice: '0.050',
+        Type: 'one-time'
+    }
+    const command = new RequestSpotInstancesCommand(input)
+    await client.send(command)
+}
