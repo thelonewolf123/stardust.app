d330b5a29319406e6a188d7fc0afa2fe383c33c6
thelonewolf123 <harishkumar.vellore@gmail.com>
2023-06-11 16:30:20 +0530

    fix

---
+++ packages/lambda/index.ts
@@ -0,0 +1,13 @@
+import {
+    APIGatewayProxyEvent,
+    APIGatewayProxyResult
+} from 'aws-lambda/trigger/api-gateway-proxy'
+export const lambdaHandler = async (
+    event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+    const queries = JSON.stringify(event.queryStringParameters)
+    return {
+        statusCode: 200,
+        body: `Queries: ${queries}`
+    }
+}
