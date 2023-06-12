c087b1c7561f2e750ef0a1ddb45ddacf0097a64c
thelonewolf123 <harishkumar.vellore@gmail.com>
2023-06-12 21:59:00 +0530

    build working!

--- packages/backend/index.ts
+++ packages/backend/index.ts
@@ -1,7 +1,14 @@
+import express from 'express'
+import bodyParser from 'body-parser'
 import { requestEc2SpotInstance } from './aws/ec2'
 
-function start() {
-    return requestEc2SpotInstance(1)
-}
+const app = express()
 
-start()
+app.use(bodyParser)
+
+app.post('/api/create_ec2_instance', (req, res) => {
+    const count = parseInt(req.body.count)
+    return requestEc2SpotInstance(count)
+})
+
+app.listen(process.env.PORT || 3000)
---
+++ scripts/build.js
@@ -0,0 +1,27 @@
+const { build } = require('esbuild')
+const path = require('path')
+// const { dependencies, peerDependencies } = require('./package.json')
+
+const sharedConfig = {
+    bundle: true,
+    minify: true
+    // external: Object.keys(dependencies).concat(Object.keys(peerDependencies))
+}
+
+const entries = {
+    backend: path.resolve(__dirname, '../packages/backend/index.ts'),
+    spot: path.resolve(__dirname, '../packages/spot/index.ts'),
+    lambda: path.resolve(__dirname, '../packages/lambda/index.ts')
+}
+
+Object.entries(entries).map((value) => {
+    const [name, path] = value
+
+    build({
+        ...sharedConfig,
+        entryPoints: [path],
+        platform: 'node', // for CJS
+        outfile: `dist/${name}.js`,
+        sourcemap: true
+    })
+})
