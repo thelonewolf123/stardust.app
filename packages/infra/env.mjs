4487d18657da50e9518872a8d940da5db60cd6fb
thelonewolf123 <harishkumar.vellore@gmail.com>
2023-08-02 09:48:59 +0530

    added pulumi

---
+++ packages/infra/env.mjs
@@ -0,0 +1,40 @@
+// src/env.mjs
+import { createEnv } from '@t3-oss/env-nextjs'
+import { z } from 'zod'
+import { configDotenv } from 'dotenv'
+
+if (process.env.NODE_ENV === 'development') {
+    configDotenv({
+        path: './../../.env.local'
+    })
+}
+
+export const env = createEnv({
+    server: {
+        MONGODB_URI: z.string().url(),
+
+        AWS_ACCESS_KEY_ID: z.string().min(1),
+        AWS_ACCESS_KEY_SECRET: z.string().min(1),
+        AWS_REGION: z.string().min(1),
+        CONTAINER_BUCKET_NAME: z.string().min(1),
+        NODE_ENV: z.enum(['development', 'production']),
+        DEPLOYMENT_ENV: z.enum(['local', 'aws']),
+        CHECKPOINT_PATH: z.string().min(1),
+        SPOT_INSTANCE_ID: z.string().min(1),
+        RABBITMQ_URL: z.string().url()
+    },
+    client: {},
+    runtimeEnv: {
+        MONGODB_URI: process.env.MONGODB_URI,
+
+        AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+        AWS_ACCESS_KEY_SECRET: process.env.AWS_ACCESS_KEY_SECRET,
+        AWS_REGION: process.env.AWS_REGION,
+        CONTAINER_BUCKET_NAME: process.env.CONTAINER_BUCKET_NAME,
+        NODE_ENV: process.env.NODE_ENV,
+        DEPLOYMENT_ENV: process.env.DEPLOYMENT_ENV,
+        CHECKPOINT_PATH: process.env.CHECKPOINT_PATH,
+        SPOT_INSTANCE_ID: process.env.SPOT_INSTANCE_ID,
+        RABBITMQ_URL: process.env.RABBITMQ_URL
+    }
+})
---
+++ packages/infra/index.ts
@@ -0,0 +1,11 @@
+import * as aws from '@pulumi/aws'
+import * as awsx from '@pulumi/awsx'
+import * as pulumi from '@pulumi/pulumi'
+
+import { env } from '../env'
+
+// Create an AWS resource (S3 Bucket)
+const bucket = new aws.s3.Bucket(env.CONTAINER_BUCKET_NAME)
+
+// Export the name of the bucket
+export const bucketName = bucket.id
