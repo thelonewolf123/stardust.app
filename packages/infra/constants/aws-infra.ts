3edb5b3d9daed1fa34df3841e8f648161e8ff72d
thelonewolf123 <harishkumar.vellore@gmail.com>
2023-08-04 01:00:33 +0530

    added security group to infra

---
+++ packages/infra/constants/aws-infra.ts
@@ -0,0 +1,17 @@
+const EC2_INSTANCE_TYPE = 't2.micro'
+const EC2_INSTANCE_NAME = 'soulForgeBaseInstance'
+const EC2_PRIVATE_KEY_NAME = 'ec2-privateKey.pem'
+const EC2_PUBLIC_KEY_NAME = 'ec2-publicKey.pub'
+const SSH_KEY_NAME = 'infra-soulbound-keypair'
+const EC2_AMI_NAME = 'soulForgeBaseAmi'
+const EC2_SECURITY_GROUP_NAME = 'soulForgeSecurityGroup'
+
+export {
+    EC2_INSTANCE_TYPE,
+    EC2_INSTANCE_NAME,
+    EC2_PRIVATE_KEY_NAME,
+    EC2_PUBLIC_KEY_NAME,
+    SSH_KEY_NAME,
+    EC2_AMI_NAME,
+    EC2_SECURITY_GROUP_NAME
+}
--- packages/infra/index.ts
+++ packages/infra/index.ts
@@ -2,34 +2,33 @@ import * as aws from '@pulumi/aws'
 import * as awsx from '@pulumi/awsx'
 import * as pulumi from '@pulumi/pulumi'
 
+import * as awsInfra from './constants/aws-infra'
 import { env } from './env'
 import { generateSshKey } from './library/ssh-keygen'
 
 const bucket = new aws.s3.Bucket(env.CONTAINER_BUCKET_NAME)
-
 export const bucketName = bucket.id
-const keyPairName = 'infra-soulbound-keypair'
 
 const privateKey = generateSshKey()
 const privateKeyBucket = new aws.s3.Bucket(env.PRIVATE_KEY_BUCKET_NAME)
 
 export const privateKeyBucketId = privateKeyBucket.id
 
-const privateKeyS3 = new aws.s3.BucketObject('ec2-privateKey.pem', {
+new aws.s3.BucketObject(awsInfra.EC2_PRIVATE_KEY_NAME, {
     key: 'privateKey.pem',
     acl: 'private',
     bucket: bucket.id,
     content: privateKey.privateKey
 })
 
-const publicKeyS3 = new aws.s3.BucketObject('ec2-publicKey.pub', {
+new aws.s3.BucketObject(awsInfra.EC2_PUBLIC_KEY_NAME, {
     key: 'publicKey.pub',
     acl: 'private',
     bucket: bucket.id,
     content: privateKey.publicKey
 })
 
-const keyPair = new aws.ec2.KeyPair(keyPairName, {
+const keyPair = new aws.ec2.KeyPair(awsInfra.SSH_KEY_NAME, {
     publicKey: privateKey.publicKey
 })
 export const sshKeyName = keyPair.keyName
@@ -53,13 +52,30 @@ const ubuntuAmi = pulumi.output(
     })
 )
 
-// Create an EC2 instance
-const instance = new aws.ec2.Instance('soulForgeBaseInstance', {
+const securityGroup = new aws.ec2.SecurityGroup(
+    awsInfra.EC2_SECURITY_GROUP_NAME,
+    {
+        description: 'Allow inbound traffic on ports 22, 80, 443'
+    }
+)
+
+for (const port of [22, 80, 443]) {
+    new aws.ec2.SecurityGroupRule(`sgRule${port}`, {
+        type: 'ingress',
+        fromPort: port,
+        toPort: port,
+        protocol: 'tcp',
+        securityGroupId: securityGroup.id,
+        cidrBlocks: ['0.0.0.0/0']
+    })
+}
+const instance = new aws.ec2.Instance(awsInfra.EC2_INSTANCE_NAME, {
     ami: ubuntuAmi.apply((ami) => ami.id),
-    instanceType: 't2.micro',
+    instanceType: awsInfra.EC2_INSTANCE_TYPE,
     keyName: keyPair.id.apply((keyName) => keyName),
+    vpcSecurityGroupIds: [securityGroup.id],
     tags: {
-        Name: 'soulForgeBaseInstance'
+        Name: awsInfra.EC2_INSTANCE_NAME
     },
     userData: `#!/bin/bash
     sudo apt-get update
@@ -72,9 +88,9 @@ const instance = new aws.ec2.Instance('soulForgeBaseInstance', {
 // Create an AMI from the instance when it is ready
 const ami = instance.id.apply(
     (id) =>
-        new aws.ec2.AmiFromInstance('soulForgeBaseAmi', {
+        new aws.ec2.AmiFromInstance(awsInfra.EC2_AMI_NAME, {
             sourceInstanceId: id,
-            name: 'soulForgeBaseAmi'
+            name: awsInfra.EC2_AMI_NAME
         })
 )
 
