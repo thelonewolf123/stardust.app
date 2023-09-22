159811abea742b155a977da023414fe63660d875
thelonewolf123 <harishkumar.vellore@gmail.com>
2023-09-22 08:31:35 +0530

    queue purge

--- scripts/dev-utils/purge.ts
+++ scripts/dev-utils/purge.ts
@@ -1,7 +1,30 @@
+import {
+    BUILD_CONTAINER,
+    DESTROY_CONTAINER,
+    NEW_CONTAINER
+} from '../../constants/queue'
+import { createQueue, getClient, queueManager } from '../../packages/core/queue'
 import redis from '../../packages/core/redis'
 
 redis.connect().then(async () => {
     // purge all keys
-    const keys = await redis.client.flushDb()
+    await redis.client.flushDb()
     await redis.client.quit()
 })
+
+const queue = [BUILD_CONTAINER, DESTROY_CONTAINER, NEW_CONTAINER]
+queue.map(async (queue) => {
+    const { onMessage, channel, cleanup } = await queueManager({
+        exchange: queue.EXCHANGE_NAME,
+        queue: queue.QUEUE_NAME,
+        routingKey: queue.ROUTING_KEY
+    })
+    process.on('SIGINT', () => cleanup())
+
+    onMessage((message) => {
+        if (!message) return
+        channel.receiver.ack(message)
+    })
+})
+
+setTimeout(() => process.exit(), 100_000)
