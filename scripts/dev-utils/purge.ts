bbf456e380f4dc782608caf671c3f2dec695156f
thelonewolf123 <harishkumar.vellore@gmail.com>
2023-09-22 09:30:02 +0530

    queue purge logs

--- scripts/dev-utils/purge.ts
+++ scripts/dev-utils/purge.ts
@@ -3,13 +3,14 @@ import {
     DESTROY_CONTAINER,
     NEW_CONTAINER
 } from '../../constants/queue'
-import { createQueue, getClient, queueManager } from '../../packages/core/queue'
+import { queueManager } from '../../packages/core/queue'
 import redis from '../../packages/core/redis'
 
 redis.connect().then(async () => {
     // purge all keys
     await redis.client.flushDb()
     await redis.client.quit()
+    console.log('Redis purged')
 })
 
 const queue = [BUILD_CONTAINER, DESTROY_CONTAINER, NEW_CONTAINER]
