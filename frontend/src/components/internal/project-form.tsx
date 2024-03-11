6296241bef796f4b8e098ada7dadeedc16628a4e
thelonewolf123 <harishkumar.vellore@gmail.com>
2024-03-11 13:31:53 +0530

    Refactor parseEnv function and update input styling

--- frontend/src/components/internal/project-form.tsx
+++ frontend/src/components/internal/project-form.tsx
@@ -59,63 +59,48 @@ type ArrayFieldsBuilderType = ReturnType<
     typeof useForm<z.infer<typeof ProjectInputSchema>>
 >
 
-function parseEnv(data: string) {
-    // Split the file content into lines
-    const lines = data.split('\n')
-
-    const envVariables: {
+function parseEnv(src: string) {
+    const obj: {
         name: string
         value: string
     }[] = []
-    let currentKey = ''
-    let isMultilineValue = false
 
-    // Iterate through each line
-    lines.forEach((line) => {
-        // Ignore comments and empty lines
-        if (line.trim() !== '' && !line.startsWith('#')) {
-            // If line starts with '"', it's a multiline value
-            if (line.startsWith('"')) {
-                // If multiline value is already in progress, add current line
-                if (isMultilineValue) {
-                    currentKey += '\n' + line.trim()
-                } else {
-                    currentKey = line.trim()
-                    isMultilineValue = true
-                }
-            } else if (isMultilineValue) {
-                // If line doesn't start with '"', but is part of multiline value, add current line
-                currentKey += '\n' + line.trim()
-            } else {
-                // Split each line into key-value pair
-                const [key, value] = line.split('=')
-                // Trim whitespace from key and value
-                if (!key || !value) return
-                const trimmedKey = key.trim()
-                let trimmedValue = value.trim()
-                // If value is wrapped in double quotes, remove them
-                if (
-                    trimmedValue.startsWith('"') &&
-                    trimmedValue.endsWith('"')
-                ) {
-                    trimmedValue = trimmedValue.substring(
-                        1,
-                        trimmedValue.length - 1
-                    )
-                }
-                // Add key-value pair to the object
-                envVariables.push({
-                    name: trimmedKey,
-                    value: trimmedValue
-                })
-                // Reset multiline value tracking
-                isMultilineValue = false
-                currentKey = ''
-            }
+    const LINE =
+        /(?:^|^)\s*(?:export\s+)?([\w.-]+)(?:\s*=\s*?|:\s+?)(\s*'(?:\\'|[^'])*'|\s*"(?:\\"|[^"])*"|\s*`(?:\\`|[^`])*`|[^#\r\n]+)?\s*(?:#.*)?(?:$|$)/gm
+
+    // Convert buffer to string
+    let lines = src.toString()
+
+    // Convert line breaks to same format
+    lines = lines.replace(/\r\n?/gm, '\n')
+
+    let match
+    while ((match = LINE.exec(lines)) != null) {
+        const key: string = match[1]
+
+        // Default undefined or null to empty string
+        let value = match[2] || ''
+
+        // Remove whitespace
+        value = value.trim()
+
+        // Check if double quoted
+        const maybeQuote = value[0]
+
+        // Remove surrounding quotes
+        value = value.replace(/^(['"`])([\s\S]*)\1$/gm, '$2')
+
+        // Expand newlines if double quoted
+        if (maybeQuote === '"') {
+            value = value.replace(/\\n/g, '\n')
+            value = value.replace(/\\r/g, '\r')
         }
-    })
 
-    return envVariables
+        // Add to object
+        obj.push({ name: key, value })
+    }
+
+    return obj
 }
 
 function ArrayFieldsBuilder<T extends ArrayFieldsBuilderType>({
