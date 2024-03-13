90cd07570d62509afde5ca8300c77d68f3fecb0b
thelonewolf123 <harishkumar.vellore@gmail.com>
2024-03-13 11:01:35 +0530

    Remove unused code and dependencies

--- proxy.go/get_ip_port.lua
+++
@@ -1,5 +0,0 @@
-local domain = ARGV[1]
-local ipPort = redis.call("HGET", "domainMap", domain)
-if ipPort then
-    return ipPort
-end
--- proxy.go/go.mod
+++
@@ -1,5 +0,0 @@
-module stardust.app/proxy
-
-go 1.21.6
-
-require github.com/go-redis/redis v6.15.9+incompatible // indirect
