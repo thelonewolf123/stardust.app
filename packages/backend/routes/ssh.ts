9df8e62bda9a6415268e9474efe79dc06a9e2470
thelonewolf123 <harishkumar.vellore@gmail.com>
2024-03-15 11:23:27 +0530

    Refactor sshHandler function to include database query

--- packages/backend/routes/ssh.ts
+++ packages/backend/routes/ssh.ts
@@ -1,7 +1,23 @@
-import { Request, Response } from 'express'
 import ExpressWs from 'express-ws'
 
-export const sshHandler: ExpressWs.WebsocketRequestHandler = (ws, req) => {
+import models from '@backend/database'
+
+export const sshHandler: ExpressWs.WebsocketRequestHandler = async (
+    ws,
+    req
+) => {
+    const id = req.params.id
+    const username = req.params.username
+    const containerSlug = `${username}/${id}`
+
+    const container = await models.Container.findOne({
+        slug: containerSlug
+    }).lean()
+
+    if (!container) {
+        return ws.close()
+    }
+
     ws.on('message', (message) => {
         console.log('received: %s', message)
         ws.send(message) // Echo the message back
