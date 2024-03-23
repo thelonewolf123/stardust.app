c5be48eb4f6c61f3135d6be9435ecbb8f7803373
thelonewolf123 <harishkumar.vellore@gmail.com>
2024-03-23 16:19:26 +0530

    Update docker proxy configuration and environment path

--- infra/scripts/index.ts
+++ infra/scripts/index.ts
@@ -7,27 +7,10 @@ sudo apt install git -y
 sudo snap install node --classic
 sudo snap install aws-cli --classic
 sudo snap install docker
-  
-# Create a systemd service for the proxy
-echo '[Unit]
-Description=Start Proxy Container
-Requires=docker.socket
-After=docker.socket
 
-[Service]
-Restart=always
-ExecStart=/usr/bin/docker start -a docker-proxy-container
-ExecStop=/usr/bin/docker stop -t 2 docker-proxy-container
+sleep 30 # wait for docker to start
 
-[Install]
-WantedBy=default.target' | sudo tee /etc/systemd/system/docker-proxy.service
-
-# Create the proxy container without running it
-sudo docker create --name docker-proxy-container -v /var/run/docker.sock:/var/run/docker.sock -p 2375:2375 -e BEARER_TOKEN='${env.REMOTE_DOCKER_PASSWORD}' -e REDIS_HOST='${env.REDIS_HOST}' docker.io/thelonewolf123/docker-proxy 2> /home/ubuntu/docker-proxy-create.log 
-
-# Enable and start the service, making the container run on boot
-sudo systemctl enable docker-proxy.service
-sudo systemctl start docker-proxy.service
+sudo docker run -d --restart always --name docker-proxy-container -v /var/run/docker.sock:/var/run/docker.sock -p 2375:2375 -e BEARER_TOKEN='${env.REMOTE_DOCKER_PASSWORD}' -e REDIS_HOST='${env.REDIS_HOST}' docker.io/thelonewolf123/docker-proxy
 
 # Install aws-cli
 sudo mkdir -p /root/.aws
@@ -42,5 +25,6 @@ sudo docker login --username AWS --password $(sudo aws ecr get-login-password --
 
 export const ec2ProxyUserData = `#!/bin/bash
 echo "Starting proxy container"
-sudo docker run -d --restart always --name reverse-proxy-container -p 80:80 -e REDIS_HOST='${env.REDIS_HOST}' docker.io/thelonewolf123/docker-proxy node /app/reverse-proxy.js 2> /home/ubuntu/reverse-proxy-create.log 
+sudo docker run -d --restart always --name reverse-proxy-container -p 8080:80 -e REDIS_HOST='${env.REDIS_HOST}' docker.io/thelonewolf123/docker-proxy node /app/reverse-proxy.js 2> /home/ubuntu/reverse-proxy-create.log 
 `
+console.log(ec2UserData)
