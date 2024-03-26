2c297e7dc88b7fdd922c9ba6fe8a2f7b7e2dbe92
thelonewolf123 <harishkumar.vellore@gmail.com>
2024-03-26 20:33:31 +0530

    Refactor lambda handler to use async/await

--- lambda/index.js
+++ lambda/index.js
@@ -1,36 +1,34 @@
 const amqplib = require('amqplib')
 
-exports.handler = (event) => {
+exports.handler = async (event) => {
     console.log(event, new Date())
 
     if (event.detail['instance-action'] === 'terminate') {
         const instanceId = event.detail['instance-id']
         console.log('Instance is going to be terminated', instanceId)
+
+        const RABBITMQ_URL = process.env.RABBITMQ_URL
+        if (!RABBITMQ_URL) {
+            throw new Error('RABBITMQ_URL environment variable is required')
+        }
+
+        const conn = await amqplib.connect(RABBITMQ_URL)
+        const ch = await conn.createChannel()
+        const q = 'spot-instance-terminate'
+        const msg = JSON.stringify({
+            instanceId: event.detail['instance-id'],
+            action: event.detail['instance-action']
+        })
+        await ch.assertQueue(q, { durable: true })
+        ch.sendToQueue(q, Buffer.from(msg))
+        console.log(' [x] Sent %s', msg)
+        await ch.close()
+        await conn.close()
+        return 'Spot instance termination event processed successfully'
+    } else {
+        console.log(
+            'Instance is not going to be terminated',
+            event.detail['instance-id']
+        )
     }
-
-    const RABBITMQ_URL = process.env.RABBITMQ_URL
-    if (!RABBITMQ_URL) {
-        throw new Error('RABBITMQ_URL environment variable is required')
-    }
-
-    amqplib.connect(RABBITMQ_URL).then((conn) => {
-        return conn
-            .createChannel()
-            .then((ch) => {
-                const q = 'spot-instance-terminate'
-                const msg = JSON.stringify({
-                    instanceId: event.detail['instance-id'],
-                    action: event.detail['instance-action']
-                })
-
-                return ch.assertQueue(q, { durable: true }).then(() => {
-                    ch.sendToQueue(q, Buffer.from(msg))
-                    console.log(' [x] Sent %s', msg)
-                    return ch.close()
-                })
-            })
-            .finally(() => conn.close())
-    })
-
-    return 'Spot instance termination event processed successfully'
 }
