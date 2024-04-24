31eb8671d2adecb9e919a64ade9b108fa75c7a2c
thelonewolf123 <harishkumar.vellore@gmail.com>
2024-04-24 15:24:44 +0530

    Update AWS EC2 exposed ports in aws-infra.ts

--- constants/aws-infra.ts
+++ constants/aws-infra.ts
@@ -4,7 +4,7 @@ export const SSH_KEY_NAME = 'stardustKeyPair'
 export const EC2_AMI_NAME = 'stardustBaseAmi'
 export const EC2_SECURITY_GROUP_NAME = 'stardustSecurityGroup'
 
-export const EC2_EXPOSED_PORTS = [22, 80, 443, 2375]
+export const EC2_EXPOSED_PORTS = [22, 80, 443]
 
 export const SSM_PARAMETER_KEYS = {
     baseAmiId: 'base-ami-id',
